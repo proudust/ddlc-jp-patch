57fc929f0b6022825f56b360d1c5d97f3da240d4
Proudust <proudust@gmail.com>
2020-02-07 14:55:11 +0900

    :wrench: ビルドスクリプトを微修正

--- tools/distribute.sh
+++ tools/distribute.sh
@@ -10,9 +10,14 @@ echo "Build Doki Doki Literature Club! JP patch ver$PATCH_VERSION for v1.1.1"
 rm -rf /tmp/ddlc-jp
 cp -fpr /tmp/ddlc /tmp/ddlc-jp
 cp -fpr `dirname $0`/../* /tmp/ddlc-jp
-/tmp/renpy/renpy.sh /tmp/renpy/launcher distribute /tmp/ddlc-jp --package Mod
 
-rm -rf /tmp/ddlc-jp-dist
+result=0
+/tmp/renpy/renpy.sh /tmp/renpy/launcher distribute /tmp/ddlc-jp --package Mod || result=$?
+if [ ! "$result" = "0" ]; then
+  exit 1
+fi
+
+rm -rf /tmp/ddlc-jp-dist ./DDLC_JP_$PATCH_VERSION ./DDLC_JP_$PATCH_VERSION.zip
 unzip -q `ls /tmp/*-dists/*-Mod.zip` -d /tmp/ddlc-jp-dist
 mv `ls -d /tmp/ddlc-jp-dist/* | head -n1` ./DDLC_JP_$PATCH_VERSION
 zip -q -r ./DDLC_JP_$PATCH_VERSION.zip ./DDLC_JP_$PATCH_VERSION
